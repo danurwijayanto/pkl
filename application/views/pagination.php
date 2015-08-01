48563435f5bbc3c83d5c7208514d5db2cedfbb3d
mobinityfx <mobinity.fx@gmail.com>
2015-08-01 11:04:50 +0700

    alhamdulillah

--- application/views/pagination.php
+++ application/views/pagination.php
@@ -4,7 +4,8 @@
 
 						foreach ($konten as $konten) { ?>	
 							<!-- Place somewhere in the <body> of your page -->
-							<h2 style="padding: 15px 0 0 20px;"><?php echo $konten['judul']?></h2>
+							<h2 style="padding: 15px 0 0 20px;"><?php echo $konten['judul']?></h2><br>
+							<p><?php echo substr( strip_tags($konten['text']), 0, 300).' .....';?></p><br>
 						<?php
 							}
 						?>
